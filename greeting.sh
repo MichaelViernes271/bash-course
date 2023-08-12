69060ae4d7bea41332fa5db319905307215e467d
MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
2023-08-13 00:07:10 +0800

    echo, comments, input, and vars commands

---
+++ greeting.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+echo "hello world"
+
+#asks for user name
+read -p "what is your name" name
+
+#prints greeting
+echo "oh! hello, ${name}"
+read
+
+exit
---
+++ variables.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+:' -this is a comment-
+discovering what are variables
+system, user, and env vars
+'
+
+echo System variable:
+echo "System var: " $SYSTEM
+echo User variable:
+echo 
