a64b9e1883f3ee9d88dc4d505eb7152d6df187dd
MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
2023-08-13 00:29:31 +0800

    completes variables.sh

--- variables.sh
+++ variables.sh
@@ -1,11 +1,17 @@
-#! /usr/bin/bash
+#! /usr/bin/bash 
 
-:' -this is a comment-
-discovering what are variables
-system, user, and env vars
-'
+
+:'-this is a comment- discovering what are variables system, user, and env vars '
 
 echo System variable:
-echo "System var: " $SYSTEM
-echo User variable:
-echo 
+echo "\$bash: " + $BASH
+echo "\$bash version: " + $BASH_VERSION
+echo "\$home: " + $HOME 
+echo "\$pwd: " + $PWD 
+
+
+#declaring system variables
+
+#rectangle area = b X h
+((recarea=8*4))
+echo rectangle area = 8 \* 4 = $recarea
